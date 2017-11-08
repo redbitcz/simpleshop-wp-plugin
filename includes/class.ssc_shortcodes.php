a82a752b51f59f7590572a5d825edc127abf5038
Václav Greif <vaclav.greif@gmail.com>
2017-11-08 10:26:00 +0100

    Fixed infinite loop in MioWEB when using the shortcode

--- includes/class.ssc_shortcodes.php
+++ includes/class.ssc_shortcodes.php
@@ -5,9 +5,15 @@ namespace SSC;
 class SSC_Shortcodes{
 
     function __construct(){
+        add_action('init',array($this,'initialize'));
+
+    }
+
+    function initialize() {
         add_shortcode('SimpleShop-form',array($this,'simple_shop_form'));
         add_shortcode('SimpleShop-content',array($this,'simple_shop_content'));
     }
+
 
     function simple_shop_form($atts){
         $url = substr($_SERVER['SERVER_NAME'],-2) === 'lc' ? 'http://form.simpleshop.czlc' : 'https://form.simpleshop.cz';
@@ -73,9 +79,109 @@ class SSC_Shortcodes{
         }
 
         // Support shortcodes inside shortcodes
+
+        // Fix for MioWEB
+        $hook = $this->remove_anonymous_object_filter('the_content','visualEditorPage','create_content');
+
         $content = apply_filters('the_content',$content);
+
+        // Add the filter back if needed
+        if ($hook)
+            add_filter($hook[0],$hook[1],$hook[2]);
+
         return $content;
 
+    }
+
+    /**
+     * An utility function to remove any hook from a class
+     * @param $tag
+     * @param $class
+     * @param $method
+     * @return array|bool|void
+     */
+    function remove_anonymous_object_filter( $tag, $class, $method )
+    {
+        $filters = $GLOBALS['wp_filter'][ $tag ];
+
+        if ( empty ( $filters ) )
+        {
+            return;
+        }
+
+        foreach ( $filters as $priority => $filter )
+        {
+            foreach ( $filter as $identifier => $function )
+            {
+                if ( is_array( $function)
+                    and is_a( $function['function'][0], $class )
+                    and $method === $function['function'][1]
+                )
+                {
+
+                    remove_filter(
+                        $tag,
+                        array ( $function['function'][0], $method ),
+                        $priority
+                    );
+                    return array($tag,array ( $function['function'][0], $method ),$priority);
+                }
+            }
+        }
+
+        return false;
+    }
+
+    function list_hooks( $hook = '' ) {
+        global $wp_filter;
+
+        if ( isset( $wp_filter[$hook]->callbacks ) ) {
+            array_walk( $wp_filter[$hook]->callbacks, function( $callbacks, $priority ) use ( &$hooks ) {
+                foreach ( $callbacks as $id => $callback )
+                    $hooks[] = array_merge( [ 'id' => $id, 'priority' => $priority ], $callback );
+            });
+        } else {
+            return [];
+        }
+
+        foreach( $hooks as &$item ) {
+            // skip if callback does not exist
+            if ( !is_callable( $item['function'] ) ) continue;
+
+            // function name as string or static class method eg. 'Foo::Bar'
+            if ( is_string( $item['function'] ) ) {
+                $ref = strpos( $item['function'], '::' ) ? new \ReflectionClass( strstr( $item['function'], '::', true ) ) : new \ReflectionFunction( $item['function'] );
+                $item['file'] = $ref->getFileName();
+                $item['line'] = get_class( $ref ) == 'ReflectionFunction'
+                    ? $ref->getStartLine()
+                    : $ref->getMethod( substr( $item['function'], strpos( $item['function'], '::' ) + 2 ) )->getStartLine();
+
+                // array( object, method ), array( string object, method ), array( string object, string 'parent::method' )
+            } elseif ( is_array( $item['function'] ) ) {
+
+                $ref = new \ReflectionClass( $item['function'][0] );
+
+                // $item['function'][0] is a reference to existing object
+                $item['function'] = array(
+                    is_object( $item['function'][0] ) ? get_class( $item['function'][0] ) : $item['function'][0],
+                    $item['function'][1]
+                );
+                $item['file'] = $ref->getFileName();
+                $item['line'] = strpos( $item['function'][1], '::' )
+                    ? $ref->getParentClass()->getMethod( substr( $item['function'][1], strpos( $item['function'][1], '::' ) + 2 ) )->getStartLine()
+                    : $ref->getMethod( $item['function'][1] )->getStartLine();
+
+                // closures
+            } elseif ( is_callable( $item['function'] ) ) {
+                $ref = new \ReflectionFunction( $item['function'] );
+                $item['function'] = get_class( $item['function'] );
+                $item['file'] = $ref->getFileName();
+                $item['line'] = $ref->getStartLine();
+
+            }
+        }
+
+        return $hooks;
     }
 
 }
