4bbc5cc47ee3db40c215b0b79ca506a3ea20d9c6
Jakub Bouček <pan@jakubboucek.cz>
2019-10-13 14:18:38 +0200

    Add back-compatibility of removed Loader
    For case if any project already using `new Loader()` to access plugin internals.

---
+++ src/Loader.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package Redbit\SimpleShop\WpPlugin
+ * @license MIT
+ * @copyright 2016-2018 Redbit s.r.o.
+ * @author Redbit s.r.o. <info@simpleshop.cz>
+ */
+
+namespace Redbit\SimpleShop\WpPlugin;
+
+/**
+ * Back-compatibility of deprecated plugin loader
+ * @package Redbit\SimpleShop\WpPlugin
+ */
+class Loader {
+	public function __construct() {
+		$class = SimpleShop::class;
+		trigger_error(
+			"Manually start of plugin is deprecated, use '$class::getInstance()' instead",
+			E_USER_DEPRECATED
+		);
+	}
+
+	/**
+	 * @param string $method
+	 * @param array $arguments
+	 *
+	 * @return mixed
+	 */
+	public function __call( $method, array $arguments ) {
+		$currentClass = self::class;
+		$class        = SimpleShop::class;
+		trigger_error(
+			"'$currentClass' is deprecated, use '$class::getInstance()->$method()' instead",
+			E_USER_DEPRECATED
+		);
+
+		return call_user_func_array( [ SimpleShop::getInstance(), $method ], $arguments );
+	}
+}
