2840e95b82f4c6d13bc26413983b125423026a4b
vasikgreif <info@vaclavgreif.cz>
2021-03-10 17:30:33 +0100

    Move email notifications to separate method, send email when the user is added to group manually from admin

--- src/Rest.php
+++ src/Rest.php
@@ -143,90 +143,7 @@ class Rest extends WP_REST_Controller {
 			add_user_to_blog( get_current_blog_id(), $user_id, 'subscriber' );
 		}
 
-		// Get the posts that have some group assigned
-		$args = [
-			'posts_status' => [ 'published', 'draft' ],
-			'meta_query'   => [
-				[
-					'key'     => '_ssc_groups',
-					'compare' => 'EXISTS',
-				],
-			],
-			'post_type'    => 'any',
-		];
-
-		$posts = get_posts( $args );
-
-		// Get the post details
-		$links = [];
-		$i     = 0;
-
-		// For each group from request
-		// foreach($request->get_param('user_group') as $group){
-		// Foreach each group
-		$SSC_group = new Group();
-		foreach ( $SSC_group->get_user_groups( $user_id ) as $group ) {
-			// Scrub through posts and check, if some of the posts has that group assigned
-			foreach ( $posts as $post ) {
-				/** @var WP_Post $post */
-				$access = $this->loader->get_access();
-				$groups = $access->get_post_groups( $post->ID );
-
-				if ( in_array( $group, $groups ) ) {
-					// Check if the post can be accessed already, if not, continue
-					$specific_date  = $access->get_post_date_to_access( $post->ID );
-					$days_to_access = $access->get_post_days_to_access( $post->ID );
-
-					if ( $specific_date && date( 'Y-m-d' ) < $specific_date ) {
-						continue;
-					}
-
-					if ( $days_to_access && $days_to_access > 0 ) {
-						continue;
-					}
-
-					// If so, get the post details and add it to the links array
-					$links[ $group ][ $i ]['title'] = $post->post_title;
-					$links[ $group ][ $i ]['url']   = get_permalink( $post->ID );
-					$i ++;
-				}
-			}
-		}
-
-		$email_enable = nl2br( $this->loader->get_settings()->ssc_get_option( 'ssc_email_enable' ) );
-
-		// It doesn't seem to make sense to send email without the links, so check first
-		if ( ( (string) $email_enable ) != '2' ) { // pokud nemame zakazano posilat mail novym clenum
-			$email_body    = nl2br( $this->loader->get_settings()->ssc_get_option( 'ssc_email_text' ) );
-			$email_subject = nl2br( $this->loader->get_settings()->ssc_get_option( 'ssc_email_subject' ) );
-			$pages         = '';
-			foreach ( $links as $groupid => $linksInGroup ) {
-				$post_details = get_post( $groupid );
-				$pages        .= '<div><b>' . $post_details->post_title . '</b></div>'
-				                 . '<ul>';
-				foreach ( $linksInGroup as $link ) {
-					$pages .= '<li><a href="' . $link['url'] . '">' . $link['title'] . '</a></li>';
-				}
-				$pages .= '</ul>';
-			}
-
-			$replaceArray = [ // pole ktera je mozne nahradit
-				'{pages}'     => $pages, // zpetna kompatibilita s v1.1
-				'{mail}'      => $email, // zpetna kompatibilita s v1.1
-				'{login}'     => $_login,
-				'{password}'  => $_password,
-				'{login_url}' => wp_login_url(),
-//                '{login}' => $_login,
-//                '{password}' => $_password,
-			];
-			$email_body   = str_replace( array_keys( $replaceArray ), array_values( $replaceArray ), $email_body );
-//            $email_body = str_replace('{pages}',$pages,$email_body);
-//            $email_body = str_replace('{mail}',$email,$email_body);
-			$headers = [ 'Content-Type: text/html; charset=UTF-8' ];
-
-			// Send the email
-			wp_mail( $email, $email_subject, $email_body, $headers );
-		}
+		$this->loader->get_access()->send_welcome_email( $user_id );
 
 		return new WP_REST_Response( [ 'status' => 'success', 'plugin_version' => SIMPLESHOP_PLUGIN_VERSION ], 200 );
 	}
@@ -245,7 +162,7 @@ class Rest extends WP_REST_Controller {
 	/**
 	 * Prepare the item for the REST response
 	 *
-	 * @param mixed $item WordPress representation of the item.
+	 * @param mixed           $item WordPress representation of the item.
 	 * @param WP_REST_Request $request Request object.
 	 *
 	 * @return mixed
