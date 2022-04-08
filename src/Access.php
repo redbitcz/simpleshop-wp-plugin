9188152c7bd037a9a10c8edabf13f2b1cc42d1da
Jakub Bouček <pan@jakubboucek.cz>
2022-04-08 11:24:37 +0200

    Fix date to fix-date-to-shortcode (fix code style)

--- src/Access.php
+++ src/Access.php
@@ -324,8 +324,8 @@ class Access {
 
 		if ( ! empty( $specific_date_to ) ) {
 			// Fix shortcode.
-			if (date('H:i:s',strtotime($specific_date_to)) == '00:00:00') {
-				$specific_date_to = date('Y-m-d',strtotime($specific_date_to)) . ' 23:59:59';
+			if ( date( 'H:i:s', strtotime( $specific_date_to ) ) == '00:00:00' ) {
+				$specific_date_to = date( 'Y-m-d', strtotime( $specific_date_to ) ) . ' 23:59:59';
 			}
 
 			// Check against the to date, this has nothing to do with groups or other settings
